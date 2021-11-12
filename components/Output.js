bdaa53d6cfc9b34f9db74a3835c98fecf5044e9a
Ates Goral <ates@magnetiq.com>
2021-11-12 01:11:13 -0500

    Don't use offscreen canvas for output. Fixes #5

--- components/Output.js
+++ components/Output.js
@@ -12,14 +12,6 @@ export class Output extends Canvas {
    */
   constructor(canvas, width, height) {
     super(canvas, width, height);
-
-    this.offscreen = document.createElement('canvas');
-    this.offscreen.width = this.width;
-    this.offscreen.height = this.height;
-
-    this.offscreenCtx = /** @type {CanvasRenderingContext2D} */ (
-      this.offscreen.getContext('2d')
-    );
   }
 
   /**
@@ -27,7 +19,6 @@ export class Output extends Canvas {
    */
   init(scanner) {
     this.clear();
-
     this.scan = scanner.scan(this.width, this.height);
   }
 
@@ -46,9 +37,7 @@ export class Output extends Canvas {
       return;
     }
 
-    this.offscreenCtx.fillStyle = color.toCss();
-    this.offscreenCtx.fillRect(pos.x, pos.y, 1, 1);
-
-    this.ctx.drawImage(this.offscreen, 0, 0);
+    this.ctx.fillStyle = color.toCss();
+    this.ctx.fillRect(pos.x, pos.y, 1, 1);
   }
 }
